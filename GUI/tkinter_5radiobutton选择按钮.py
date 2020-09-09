a8f5919f1533ff61c8299b7d0f6f0269b374c401
wyc001208 <wyc20001208@126.com>
2020-09-09 09:06:22 +0800

    radiobutton

--- GUI/tkinter_5radiobutton选择按钮.py
+++ GUI/tkinter_5radiobutton选择按钮.py
@@ -1,27 +1,19 @@
-from tkinter import*
+from tkinter import *
 
 win=Tk()
 win.title('radiobutton')
 win.geometry('900x400')
 
-var=StringVar()
-l1=Label(win,text='Which is the correct pronunciaton')
-l1.pack()
-l=Label(win,bg='yellow',width=40,text='please choose one of these following selections')
+l=Label(win,width=20,height=2,bg='yellow',text='Please choose one of these selections')
 l.pack()
 
-def print_selectionA():
-    l.config(text='Congratulations!',bg='green')
-def print_selectionB():
-    l.config(text='What are you F**king thingking about?',bg='red')
-def print_selectionC():
-    l.config(text='Serious?????',bg='purple')
-r1=Radiobutton(win,text='[paɪθɑn]',command=print_selectionA)
-r2=Radiobutton(win,text='[paɪθen]',command=print_selectionB)
-r3=Radiobutton(win,text='[I love xjx]',command=print_selectionC)
-
+var=StringVar()
+def print_selection():
+    l.config(text='you have selected'+var.get())
+r1=Radiobutton(win,text='A:[paiθan]',variable=var,value='A',command=print_selection)
+r2=Radiobutton(win,text='A:[paiθen]',variable=var,value='B',command=print_selection)
+r3=Radiobutton(win,text='A:[paiθin]',variable=var,value='C',command=print_selection)
 r1.pack()
 r2.pack()
 r3.pack()
-
 win.mainloop()
